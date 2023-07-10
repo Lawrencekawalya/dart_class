23baf64d1d42908cf27fa4ca74569c1b0d3995d9
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-10 16:35:08 +0300

    first console project in internship

---
+++ lib/dart_intro.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "2";
+  String university = "BSU";
+  int age = 50;
+  bool isAtUni = false;
+
+  print('University\'s:  ${university} \nName: $name \nAge: $age ');
+
+}
