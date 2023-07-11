e7ebbc8250023fd7649d45a68d32632c6262b40c
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-11 12:21:28 +0300

    learn about arithmetic operators

---
+++ lib/operators.dart
@@ -0,0 +1,17 @@
+// void main()  => print("Welcome to  class");
+void main() {
+  // String name = "BISHOP STUART UNIVERSTY";
+  // print(name);
+
+  //  Arithmetic operators
+  int a = 8;
+  int b = 3;
+  print("Addition: a +  b  = ${a + b}");
+  print("Addition: a -  b  = ${a - b}");
+  print("Addition: a  * b  = ${a * b}");
+  print("Addition: a /  b  = ${a / b}");
+  print("Addition: a %  b  = ${a % b}");
+
+  //  Comparison operators
+  //  Logical operators
+}
