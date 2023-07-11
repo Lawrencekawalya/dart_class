de68a9dc9acd54f6f4fba31e8cf36f84239d1b9e
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-11 14:39:42 +0300

    operators 2.39 11.07.23

--- lib/operators.dart
+++ lib/operators.dart
@@ -1,17 +1,26 @@
 // void main()  => print("Welcome to  class");
 void main() {
-  // String name = "BISHOP STUART UNIVERSTY";
+  String university = "BISHOP STUART UNIVERSTY";
   // print(name);
 
   //  Arithmetic operators
   int a = 8;
   int b = 3;
-  print("Addition: a +  b  = ${a + b}");
-  print("Addition: a -  b  = ${a - b}");
-  print("Addition: a  * b  = ${a * b}");
-  print("Addition: a /  b  = ${a / b}");
-  print("Addition: a %  b  = ${a % b}");
+  // print("Addition: a +  b  = ${a + b}");
+  // print("Addition: a -  b  = ${a - b}");
+  // print("Addition: a  * b  = ${a * b}");
+  // print("Addition: a /  b  = ${a / b}");
+  // print("Addition: a %  b  = ${a % b}");
 
   //  Comparison operators
+  // print(a < b);
+  // print(a == b);
+  // print("BSU" != "bsu");
+  // print(2 <= 3);
+
+  if (university == "BISHOP STUART") {
+    print("I am a student of BSU");
+  }
+
   //  Logical operators
 }
