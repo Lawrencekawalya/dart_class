675a9b9bf43e2a2ba3df70de8330c67834a1f24b
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-12 14:30:06 +0300

    loops and lists 12.7.23

---
+++ lib/loops.dart
@@ -0,0 +1,95 @@
+void main() {
+  var list1 = [
+    3,
+    "5",
+    "BSU",
+    true
+  ];
+  List<dynamic> mixedList = [
+    "1",
+    3,
+    "four",
+    true,
+    false
+  ];
+  List<String> strings = [
+    "BSU",
+    "1",
+    "2"
+  ];
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4
+  ];
+  List<bool> booleans = [
+    true,
+    false,
+  ];
+  List<List<int>> numberLists = [
+    [
+      1,
+      3,
+      4
+    ],
+    [
+      3,
+      5,
+      5
+    ]
+  ];
+
+  // print(booleans.reversed);
+  // print(numbers.reversed);
+  // print(strings.contains("1"));
+  // print(list1.first);
+  // print(list1[0] == list1.first); // same as list1.first
+  // print(mixedList.length);
+  // print(strings[0][0]);
+  // print(strings[0][1]);
+  // print(strings.first[2]);
+
+  //  while loops
+  // int i = 10;
+  // var myList = [];
+  // while (i <= 20) {
+  //   myList.add(i);
+  //   i++;
+  // }
+
+  // print(myList);
+
+  //  odd numbers between 30 and 40
+  // List<String> oddNumStrings = [];
+  // int i = 30;
+  // while (i <= 40) {
+  //   if (i % 2 != 0) {
+  //     oddNumStrings.add(i.toString());
+  //   }
+  //   i++;
+  // }
+  // print(oddNumStrings);
+
+  // for (int i = 0; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  // for each loop
+  // mixedList.forEach((i) {
+  //   print(i);
+  // });
+
+  // mixedList.forEach((element) => print(element));
+
+  // for (var i in strings) {
+  //   print(i);
+  // }
+
+  int i = 0;
+
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
+}
