5865b75f4ecea6b365746b890a01f7b2e8cbbbd8
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-14 16:10:00 +0300

    we have studied functions and some little functions with parameters

--- lib/functions.dart
+++ lib/functions.dart
@@ -5,17 +5,35 @@ void main() {
   // myFunc();
 
   //  call getUserNames function
-  getUserNames();
+  // getUserNames();
 
   //  call getAge function
-  getAge();
+  // getAge();
   // print("My name is $globalName and my age is $globalAge");
+
+  //  call getName function
+  // String myName = getName();
+  // print("My Name is $myName");
+
+  // int myAge = getMyAge();
+  // print("");
+  // print("My Name is $myName and I am $myAge years old");
+}
+
+String getName() {
+  print("Who are you?");
+  var name = stdin.readLineSync();
+  return name ?? "";
+}
+
+int getMyAge() {
+  print("How old are you?");
+  return int.parse(stdin.readLineSync() ?? "0");
 }
 
 void myFunc() {
   print("my function is awesome");
 }
-
 
 void getUserNames() {
   print("Who are you?");
